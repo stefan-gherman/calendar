0889018f90fa8bffb2e3c0d4150466ad3a726e27
Stefan Gherman <scgheman@gmail.com>
2019-11-22 21:49:01 +0200

    Add another check for hours.
    Hours that start with 0x are considered unsuitable, however 12AM parsed from an input as '0' is considered correct.

--- checks.py
+++ checks.py
@@ -8,13 +8,13 @@ def check_menu_option(input_option):
 
 def check_valid_hour(hour):
     digs = ['1' , '2' , '3', '4', '5', '6', '7', '8', '9', '0']
-    if hour.startswith('0') == True:
+    if hour.startswith('0') == True and len(hour) > 1:
         return False
     else:    
         for char in hour:
             if char not in digs:
                 return False
+    return True            
 
 def check_valid_range(hour_start, hour_end):
     pass
-
