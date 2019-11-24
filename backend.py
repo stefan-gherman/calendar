d380c079674406b660c56b79131045b8ba43eeba
Stefan Gherman <scgheman@gmail.com>
2019-11-25 00:47:04 +0200

    Add ability to schedule new meetings.

--- backend.py
+++ backend.py
@@ -1,6 +1,8 @@
 from ui import print_message, show_schedule
-from checks import check_hour_combined
+from checks import check_hour_combined, check_duration, check_end_hour_combined
 
+START_HOUR = 0
+END_HOUR = 1
 def dummy_return_list():
     return [[9,10,'Meeting_A'],[11,13,'Presentation'],[14,16,'Ending']]
 dummy_schedule = dummy_return_list()    
@@ -9,23 +11,31 @@ def kill():
     quit()
 
 def schedule_meeting(schedule):
-    START_HOUR = 0
-    END_HOUR = 1
-    print_message('Schedule a new meeting')
+    print_message('Schedule a new meeting.')
     title = get_input('Meeting Title: ')
-    start_hour = get_input('Start Hour: ')
+    start_hour = handle_hour()
+       
+    start_hour = int(start_hour)
+    duration = handle_duration()
+    end_hour = start_hour + duration
+    while check_end_hour_combined(end_hour, END_HOUR, dummy_schedule) == None:
+        duration = handle_duration()
+        end_hour = start_hour + duration
+    print_message('\nNew Meeting Created: {startHour} - {endHour} {Title}\n'.format(startHour = start_hour, 
+                                                                                    endHour = end_hour, 
+                                                                                    Title = title))    
+    dummy_schedule.append([start_hour, end_hour, title])
 
-    while check_hour_combined(start_hour,START_HOUR, dummy_schedule) == False:
-       start_hour = get_input('Start Hour: ')
-       
-    start_hour = int(start_hour) 
-       
 def cancel_meeting(schedule):
-    pass
+    print_message('Cancel a meeting.')
 
 def view_meeting(schedule):
+    schedule = sort_schedule(schedule)
     show_schedule(schedule)
     
+def sort_schedule(schedule):
+    schedule = sorted(schedule, key = lambda x: x[START_HOUR])
+    return schedule
 
 def get_input(prompt):
     option = input(prompt)
@@ -39,3 +49,19 @@ def handle_option(option):
     if option == 'q':
         return option_handler[option]()
     return option_handler[option](dummy_schedule)
+
+def handle_duration():
+    duration = get_input('Meeting Duration: ')
+    while check_duration(duration)  == None:
+        duration = get_input('Meeting Duration: ')
+    print_message('\n')
+    duration = int(duration) 
+    return duration   
+
+def handle_hour():
+    start_hour = get_input('Start Hour: ')
+
+    while check_hour_combined(start_hour,START_HOUR, dummy_schedule) == None:
+       start_hour = get_input('Start Hour: ')  
+    print_message('\n')
+    return int(start_hour)  
--- checks.py
+++ checks.py
@@ -12,14 +12,32 @@ def check_menu_option(input_option):
     return True
 
 def check_valid_hour(hour):
+   
+    if check_int(hour) == None:
+        return None
+    else:
+        return int(hour)   
+
+def check_int(string):
+    if string.startswith('0') == True and len(string) > 1:
+        return None
     digs = ['1' , '2' , '3', '4', '5', '6', '7', '8', '9', '0']
-    if hour.startswith('0') == True and len(hour) > 1:
-        return False
-    else:    
-        for char in hour:
-            if char not in digs:
-                return False
-    return int(hour)            
+    for char in string:
+        if char not in digs:
+                return None
+    return int(string) 
+
+def check_duration(duration):
+    if check_int(duration) == None:
+        print_message('Unparsable input.\n')
+        return None
+    else:
+        duration = check_int(duration)
+        if duration > 2 or duration < 1:
+            print_message('Duration is diferent from 1 or 2. Try again.\n')
+            return None
+        else:
+            return duration            
 
 def search_in_between(hour, schedule):
         for i in range(len(schedule)):
@@ -38,7 +56,7 @@ def check_hour_combined(hour,datatype, schedule):
 
     hour_modif = check_valid_hour(hour)
 
-    if hour_modif == False:
+    if hour_modif == None:
         print_message('Unparsable input!\n')
         return hour_modif
 
@@ -47,11 +65,19 @@ def check_hour_combined(hour,datatype, schedule):
             if search_in_between(hour_modif,schedule) == True:
                 if datatype == START_HOUR:
                     print_message('Meeting start time overlaping with another meeting, try again.\n')
-                return False
+                return None
             else:
                 return hour_modif    
 
         else:
             if datatype == START_HOUR:
                 print_message('There is already a meeting starting at that time, try again.\n')
-                return False        
+                return None
+
+def check_end_hour_combined(hour, datatype, schedule):
+    if search_in_between(hour,schedule) == True:
+        if datatype == END_HOUR:
+            print_message('Ending hour overlaps with another meeting, try changing the duration.\n')
+            return None
+    else:
+        return hour        
